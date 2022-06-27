23b876d56f4f87b1e0a1925ba6797dc4455c38a9
Iryna Hrynda <hryndairyna1@gmail.com>
2022-06-27 10:35:55 +0300

    initial commit

---
+++ src/main/java/com/example/users_and_articles/UsersAndArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.example.users_and_articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndArticlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersAndArticlesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/users_and_articles/UsersAndArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.users_and_articles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAndArticlesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
